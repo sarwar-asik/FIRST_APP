1f9c4e46038dd7f7a335a5e92bd49930561349d9
sarwar-asik <shasik2021@gmail.com>
2023-02-23 15:54:32 +0600

    img added

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function Header() {
   return (
     <View style={style.containers}>
+      <Text style={style.text}>Home</Text>
+      <Text style={style.text}>Products</Text>
+      <Text style={style.text}></Text>
       <Text style={style.text}>Headers</Text>
       <Text style={style.text}>Headers</Text>
-      <Text style={style.text}>Headers</Text>
-      <Text style={style.text}>Headers</Text>
-      <Text style={style.text}>Headers</Text>
+      <StatusBar style="auto"></StatusBar>
     </View>
   );
 }
 
 const style = StyleSheet.create({
   containers: {
-    display: "flex",
+    flex: 2,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "black",
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,13 +1,26 @@
 import React from "react";
 
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StatusBar, StyleSheet } from "react-native";
 import AppOne from "../../AppOne";
 export default function Home() {
   return (
     <View>
       <Text>Welcome in Home page</Text>
+
+      <Image
+        style={styles.logo}
+        source={require("../../assets/appimagepool.webp")}
+      />
       <AppOne />
-      <Image source={{}} />
+
+      <StatusBar style="auto"></StatusBar>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 500,
+    width: 400,
+  },
+});
